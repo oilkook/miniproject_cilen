767d0fa1c3af7e890cde74d7a12cbac47590d6d5
oilkook <s6135512017@phuket.psu.ac.th>
2022-04-15 23:00:49 +0700

    login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -29,27 +29,49 @@ export default function Login({ token }) {
     }
 
     const loginForm = () => (
-        <div className={styles.gridContainer}>
+        <div className={styles.Container}>
+            <div class="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <form class="space-y-6" action="#">
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to Repair Man</h5>
             <div>
-                Username:
-            </div>
-            <div>
+                <label for="Username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Your Username</label>
                 <input type="text"
                     name="username"
                     placeholder="username"
                     onChange={(e) => setUsername(e.target.value)}
-                />
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 
+                    dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
             </div>
             <div>
-                Password:
-            </div>
-            <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
                 <input type="password"
                     name="password"
-                    placeholder="password"
-                    onChange={(e) => setPassword(e.target.value)} />
+                    placeholder="••••••••"
+                    onChange={(e) => setPassword(e.target.value)} 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 
+                    dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
             </div>
+            <div class="flex items-start">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required></input>
+                </div>
+                <div class="ml-3 text-sm">
+                <label for="remember" class="font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+            </div>
+            
         </div>
+        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={login} >Login</button>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered? <a href="/register" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+        </div>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Status: {status}
+        </div>    
+        </form></div>
+          </div>
+      
     )
 
     const copyText = () => {
@@ -63,19 +85,7 @@ export default function Login({ token }) {
             </Head>
             <div className={styles.container}>
                 <Navbar />
-                <h1>Login</h1>
-                <div><b>Token:</b> {token.substring(0, 15)}...
-                <button onClick={copyText}> Copy token </button>
-                </div>
-                <br/>
-                <div>
-                    Status:  {status}
-                </div>
-                <br />
                 {loginForm()}
-                <div>
-                    <button onClick={login}>Login</button>
-                </div>
             </div>
         </Layout>
     )
@@ -84,3 +94,4 @@ export default function Login({ token }) {
 export function getServerSideProps({ req, res }) {
     return { props: { token: req.cookies.token || "" } };
 }
+
