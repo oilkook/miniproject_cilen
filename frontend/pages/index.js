a69ec5e93dc29183e4ff3a3901ce40e3483846d5
oilkook <s6135512017@phuket.psu.ac.th>
2022-04-15 23:05:11 +0700

    Home,login,regis

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,16 +6,54 @@ import styles from '../styles/Home.module.css'
 export default function Home({ token }) {
  
   return (
-    <Layout>
+    <div>
+      <Navbar />
+      <Layout>
     <Head>
         <title>First Page</title>
     </Head>
-    <div className={styles.container}>
-        <Navbar />
-        <h1>Home page</h1>
-        No login required!
+    <div className={styles.container}> 
+        
+        <div className="flex flex-wrap">
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+<a href="#">
+<img class="rounded-t-lg w-full" src="https://img.kaidee.com/prd/20180504/337946275/b/9bd13715-3ad3-4564-8da6-30ec643a616c.jpg"  alt="" />
+</a>
+<div class="p-5">
+<a href="#">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ระบบแจ้งซ่อมออนไลน์</h5>
+</a>
+<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">ผู้แจ้งซ่อมสามารถแจ้งผ่านระบบออนไลน์ และสามารถตรวจสอบสถานะการแจ้งซ่อม ว่างานที่แจ้งอยู่ในสถานะใด เช่น แจ้งซ่อม/ รอตรวจสอบ / ดำเนินการ / ส่งซ่อม เป็นต้น</p>
+<a href="/repair" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+แจ้งซ่อม
+    <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+</a>
+</div>    
+</div>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <br></br>
+          <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+<a href="#">
+<img class="rounded-t-lg w-full" src="https://inwfile.com/s-cn/qw53a5.jpg"  alt="" />
+</a>
+<div class="p-5">
+<a href="#">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ประวัติการซ่อม</h5>
+</a>
+<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">ฝ่ายช่างสามารถตรวจสอบประวัติการซ่อมของอุปกรณ์ เพื่อความสะดวกในการวิเคราะห์ปัญหาของอุปกรณ์ที่ผ่านมา</p>
+<a href="/repair_service" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+ข้อมูลการซ่อม 
+    <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+</a>
+</div>    
+</div>
+
+        </div>
+        
     </div>
 </Layout>
+    </div>
+    
   )
 }
 
@@ -24,3 +62,4 @@ export function getServerSideProps({ req, res }) {
   // console.log('req: ', req.headers)
   return { props: { token: req.cookies.token || "" } };
 }
+
--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
@@ -38,66 +37,59 @@ export default function Register({ token }) {
     }
 
     const registerForm = () => (
-        <div className={styles.gridContainer}>
-            <div>
-                Username:
-            </div>
-            <div>
+        <div className={styles.Container}>
+            <div class="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <form class="space-y-6" action="#">
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign Up</h5>
+        <div>
+                <label for="Username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Your Username</label>
                 <input type="text"
                     name="username"
                     placeholder="username"
                     onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div>
-                Email:
-            </div>
-            <div>
-                <input type="email"
-                    name="email"
-                    placeholder="email"
-                    onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div>
-                Password:
-            </div>
-            <div>
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 
+                    dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+        </div>
+        <div>
+                <label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Your Email</label>
+                <input type="text"
+                    name="Email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 
+                    dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+        </div>
+        <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
                 <input type="password"
                     name="password"
                     placeholder="password"
-                    onChange={(e) => setPassword(e.target.value)} />
-            </div>
-
+                    onChange={(e) => setPassword(e.target.value)} 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 
+                    dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+        </div>   
+            
+        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={register} >Register</button>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Status: {status}
+        </div>        
+        </form></div>
         </div>
     )
 
 
-    return (
-        <Layout>
+    return (<div>
+        <Navbar />
+    <Layout>
             <Head>
                 <title>Register</title>
             </Head>
-            <div className={styles.container}>
-                <Navbar />
-                <h1>Register</h1>
-                <div><b>Token:</b> {token.substring(0, 15)}...
-                <button
-                        onClick={() => { navigator.clipboard.writeText(token) }}>
-                        Copy token
-                </button>
-                </div>
-                <br />
-            Status:  {status}
-                <br /><br />
-                <div className={styles.content}>
-                    {registerForm()}
-                </div>
-
-                <div>
-                    <button onClick={register}>Register</button>
-                </div>
+            <div className={styles.container}>               
+                {registerForm()}
             </div>
         </Layout>
+        </div>
+        
     )
 }
 
